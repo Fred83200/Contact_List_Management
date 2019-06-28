04ea2fcecf5d636f86afc8b24fe3fe010df93d39
frederic jonquieres <fjonquieres83@gmail.com>
2019-06-28 03:36:25 +0100

    added manager into users table instead

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_115411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "manager", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
